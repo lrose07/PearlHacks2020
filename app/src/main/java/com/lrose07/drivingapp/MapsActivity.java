8a31086b81eceb8df46c7f1f1903fa8a1975052c
Lauren Rose <larose@radford.edu>
2020-02-22 17:32:32 -0500

    map fix

--- app/src/main/java/com/lrose07/drivingapp/MapsActivity.java
+++ app/src/main/java/com/lrose07/drivingapp/MapsActivity.java
@@ -1,27 +1,45 @@
 package com.lrose07.drivingapp;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
+    private static final int REQUEST_LOCATION = 123;
+
+    private FusedLocationProviderClient fusedLocationClient;
+
+    Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        logger.log(Level.INFO, "made it inside onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        logger.log(Level.INFO, mapFragment.toString());
         mapFragment.getMapAsync(this);
     }
 
@@ -39,9 +57,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        logger.log(Level.INFO, "something");
+
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        if (ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED
+                && (ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED)) {
+
+            ActivityCompat.requestPermissions( this, new String[] {  android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION  }, REQUEST_LOCATION);
+        }
+
+        System.out.println(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED);
+
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+
+            fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        handleCurrentLocation(location);
+                    }
+                }
+            });
+        }
+    }
+
+    private void handleCurrentLocation(Location loc) {
+        LatLng currentLatLng = new LatLng(loc.getLatitude(), loc.getLongitude());
+
+        mMap.addMarker(new MarkerOptions().position(currentLatLng).title("Test"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatLng));
     }
 }
